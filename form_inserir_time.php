cea806670aad2b7c73dc08d1820ac53391ee7343
Frederico Drumond <fredrumond@gmail.com>
2016-06-13 17:31:38 -0300

    [RMV] Removendo arquivos antigos

--- form_inserir_time.php
+++
@@ -1,8 +0,0 @@
-<h1>Frrmulario para inserir time </h1>
-<form method="POST" action="actions/inserir_time.php">
-  <span>Nome time</span>
-  <input type="text" id="nome_time" name="nome_time">
-  <span>Tecnico time</span>
-  <input type="text" id="tecnico_time" name="tecnico_time">
-  <button class="btn btn-primary" type="submit" >Inserir</button>
-</form>
